fd8171541ef6982cc0de11a16e7ab3e860e075ed
Liliwaukee <lilimmvisual@gmail.com>
2018-01-31 00:29:37 -0600

    agregando estilos al chatbot

--- js/bot.js
+++ js/bot.js
@@ -1,24 +1,24 @@
 var questionNum = 0;													// keep count of question, used for IF condition.
-var question = '<h1>what is your name?</h1>';				  // first question
+var question = '<h3>Bienvenid@ ¿Cómo te llamas?</h3>';				  // first question
 
 var output = document.getElementById('output');				// store id="output" in output variable
 output.innerHTML = question;													// ouput first question
 
 function bot() {
     var input = document.getElementById("input").value;
-    console.log(input);
+    
 
     if (questionNum == 0) {
-    output.innerHTML = '<h1>hello ' + input + '</h1>';// output response
+    output.innerHTML = '<h3>Hola ' + input + '</h3>';// output response
     document.getElementById("input").value = "";   		// clear text box
-    question = '<h1>how old are you?</h1>';			    	// load next question
+    question = '<h3>¿Cuántos años tienes?</h3>';			    	// load next question
     setTimeout(timedQuestion, 2000);									// output next question after 2sec delay
     }
 
     else if (questionNum == 1) {
-    output.innerHTML = '<h1>That means you were born in ' + (2018 - input) + '</h1>';
+    output.innerHTML = '<h3>Eso significa que naciste en ' + (2018 - input) + '</h3>';
     document.getElementById("input").value = "";
-    question = '<h1>where are you from?</h1>';
+    question = '<h3>¿De dónde eres?</h3>';
     setTimeout(timedQuestion, 2000);
     }
 }
